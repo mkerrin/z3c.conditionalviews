3e6080350772bccaad871d8e743e63713ed9c3f3
mkerrin <mkerrin@62d5b8a3-27da-0310-9561-8e5933582275>
2008-02-12 19:08:22 +0000

    Bump version number, and remove deprecation warnings.
    git-svn-id: svn://svn.zope.org/repos/main/z3c.conditionalviews/trunk@83766 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = "z3c.conditionalviews",
-    version = "1.0b",
+    version = "1.0b1",
     author = "Michael Kerrin",
     author_email = "michael.kerrin@openapp.ie",
     url = "http://svn.zope.org/z3c.conditionalviews/",
-    description = "Validation mechanism for conditional HTTP requests.",
+    description = open("README.txt").read(),
     long_description = (
-        open("README.txt").read() +
+        open("src/z3c/conditionalviews/README.txt").read() +
         "\n\n" +
         open("CHANGES.txt").read()),
     license = "ZPL 2.1",
@@ -21,7 +21,7 @@ setup(
                         "zope.schema"],
     extras_require = dict(test = ["zope.app.testing",
                                   "zope.app.zcmlfiles",
-                                  "zope.app.securitypolicy",
+                                  "zope.securitypolicy",
                                   ]),
 
     include_package_data = True,
